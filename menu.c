0dbab8d15af0cca8e05097be49a60edf40640259
vero19 <vero19@users.noreply.github.com>
2013-09-02 14:49:51 -0600

    Modificacion funcion agregarAmigos
    Se implementa un struct para guardar datos que van a ser almacenados en el archivo
    
    Se agrega la funcion para que pueda agregar mas amigos sin salirse de la opcion

--- menu.c
+++ menu.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
+//Prueba para realizar commint a un archivo ya existente
 
 /*struct amigos
 se define un struct para almacenar los datos
